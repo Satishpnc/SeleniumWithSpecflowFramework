7fb7cad0631a402ff6e386461007013e408d83ad
Satishpnc <pncsatish@gmail.com>
2017-11-21 17:05:29 +1100

    Commited POC project

---
+++ SeleniumWithSpecflowFramework/Pages/HomePage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumWithSpecflowFramework.DriverBase;
+using SeleniumWithSpecflowFramework.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumWithSpecflowFramework.Pages
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(this, new RetryingElementLocator(driver, TimeSpan.FromSeconds(10)));
+        }
+        
+        //[FindsBy(How = How.CssSelector, Using = "div[class^='MenuItem_root_QGB MenuItem_rootMain']")]
+        //public IList<IWebElement> mainMenuItems;
+        
+        [FindsBy(How = How.CssSelector, Using = "span[class^='MenuItem_text']")]
+        public IList<IWebElement> menuItems;
+
+        public void navigateToRacing()
+        {
+            List<IWebElement> menuItemsList = new List<IWebElement>(menuItems);
+
+            IWebElement racingElement = Utils.returnWebElementByText(menuItemsList, "RACING");
+
+            if (racingElement == null)
+            {
+                Assert.Fail("Unable to find Racing link in home page");
+            }
+            else
+            {
+                racingElement.Click();
+            }
+        }
+    }
+}
---
+++ SeleniumWithSpecflowFramework/SenarioHooks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using SeleniumWithSpecflowFramework.DriverBase;
+using SeleniumWithSpecflowFramework.Steps;
+
+namespace SeleniumWithSpecflowFramework
+{
+    [Binding]
+    public sealed class SenarioHooks
+    {
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            WebDriver.quitWebDriver(HorseRacingBetSteps.driver);
+        }
+    }
+}
